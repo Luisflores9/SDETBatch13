62cc964f96d33685f278e718372cafce512d2eb1
Luisflores9 <Luis.fsanchez09@gmail.com>
2022-08-22 12:33:44 -0400

    Initial Version

---
+++ CucumberBatch13/src/test/java/runners/RunnerClass.java
@@ -0,0 +1,16 @@
+package runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        //features we use to provide the path of all the features file
+        features = "src/test/resources/features/Login.feature",
+
+        glue = "steps"
+)
+public class RunnerClass {
+}
---
+++ CucumberBatch13/src/test/java/steps/AnotherStepsPage.java
@@ -0,0 +1,11 @@
+package steps;
+
+import io.cucumber.java.en.When;
+
+public class AnotherStepsPage {
+    @When("user enters invalid username and password")
+    public void user_enters_invalid_username_and_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
